90c24a36a84d355d2fc1ca37cea77d7e54b87387
Dor Duchovni <dord@wix.com>
2020-03-26 09:32:11 +0200

    add types

--- src/EditableListItem/EditableListItem.uni.driver.d.ts
+++ src/EditableListItem/EditableListItem.uni.driver.d.ts
@@ -1,7 +1,12 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
+import { InputUniDriver } from '../Input/Input.uni.driver';
+import { IconButtonUniDriver } from '../IconButton/IconButton.uni.driver';
+import { TooltipUniDriver } from '../Tooltip/TooltipNext/Tooltip.uni.driver';
 
 export interface EditableListItemUniDriver extends BaseUniDriver {
-  getCountText(): Promise<string>;
-  clickButton(): Promise<void>;
-  getButtonText(): Promise<string>;
+  inputDriver: InputUniDriver;
+  approveButtonDriver: IconButtonUniDriver;
+  cancelButtonDriver: IconButtonUniDriver;
+  cancelButtonTooltipDriver: TooltipUniDriver;
+  approveButtonTooltipDriver: TooltipUniDriver;
 }
--- src/EditableListItem/index.d.ts
+++ src/EditableListItem/index.d.ts
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import { InputStatus } from '../Input';
+
+export enum EditableListItemSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+}
 
 export interface EditableListItemProps {
   dataHook?: string;
   className?: string;
-  buttonText?: string;
+  placeholder?: string;
+  onApprove(value: string): void;
+  onCancel(): void;
+  cancelButtonTooltip?: string;
+  approveButtonTooltip?: string;
+  size?: EditableListItemSize;
+  status?: InputStatus;
+  statusMessage?: string;
 }
 
-export default class EditableListItem extends React.PureComponent<EditableListItemProps>{}
+export default class EditableListItem extends React.PureComponent<
+  EditableListItemProps
+> {}
