6de35b95a60dd30485b4a61bf1f9cb869096ef07
Jonathan Ohayon <jonathano@wix.com>
2020-06-16 16:56:45 +0300

    Added type definitions

--- src/Dropzone/index.d.ts
+++ src/Dropzone/index.d.ts
@@ -3,7 +3,10 @@ import * as React from 'react';
 export interface DropzoneProps {
   dataHook?: string;
   className?: string;
-  buttonText?: string;
+  onDrop(files: File[]): void;
 }
 
-export default class Dropzone extends React.PureComponent<DropzoneProps>{}
+export default class Dropzone extends React.PureComponent<DropzoneProps> {
+  static Overlay: React.FC;
+  static Content: React.FC;
+}
