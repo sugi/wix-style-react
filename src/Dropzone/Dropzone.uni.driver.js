dedd68f99898f3f25bbea38cd867004dc1a53267
Jonathan Ohayon <jonathano@wix.com>
2020-06-18 16:59:12 +0300

    Added documentation for dropzone unidriver

--- src/Dropzone/Dropzone.uni.driver.js
+++ src/Dropzone/Dropzone.uni.driver.js
@@ -7,8 +7,13 @@ export const dropzoneDriverFactory = (base, body) => {
   return {
     ...baseUniDriverFactory(base, body),
 
+    /** Returns the content compound element inside the dropzone. */
     getContentElement: () => base.$(`[data-hook=${dataHooks.dropzoneContent}]`),
+
+    /** Returns the overlay compound element inside the dropzone. */
     getOverlayElement: () => base.$(`[data-hook=${dataHooks.dropzoneOverlay}]`),
+
+    /** Simulates a drop event with the given files. */
     dropFiles: files =>
       // eslint-disable-next-line no-restricted-properties
       base.getNative().then(el =>
