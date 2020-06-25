8a2837a5d5858eda8a48e513e31b02c0e89cae0b
Mai Barishansky <maib@wix.com>
2020-06-25 17:10:12 +0300

    fix uni driver

--- src/NoBorderInput/NoBorderInput.uni.driver.js
+++ src/NoBorderInput/NoBorderInput.uni.driver.js
@@ -1,18 +1,15 @@
 import inputUniDriverFactory from '../Input/Input.uni.driver';
 import DataHooks from './dataHooks';
 
-const noBorderInputUniDriverFactory = ({ base, body }) => {
-  const inputDriver = inputUniDriverFactory({
-    base,
-    body,
-  });
+const noBorderInputUniDriverFactory = (base, body) => {
+  const inputDriver = inputUniDriverFactory(base, body);
 
   return {
     ...inputDriver,
     getLabel: async () =>
-      await base.$$(`[data-hook="${DataHooks.label}"]`).textContent,
+      await base.$(`[data-hook="${DataHooks.label}"]`).text(),
     getStatusMessage: async () =>
-      await base.$$(`[data-hook="${DataHooks.statusMessage}"]`).textContent,
+      await base.$(`[data-hook="${DataHooks.statusMessage}"]`).text(),
   };
 };
 
--- src/NoBorderInput/test/NoBorderInput.spec.js
+++ src/NoBorderInput/test/NoBorderInput.spec.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import noBorderInputDriverFactory from '../NoBorderInput.driver';
-import { createRendererWithDriver, cleanup } from '../../../test/utils/unit';
+import noBorderInputUniDriverFactory from '../NoBorderInput.uni.driver';
+import {
+  createRendererWithDriver,
+  createRendererWithUniDriver,
+  cleanup,
+} from '../../../test/utils/unit';
 import NoBorderInput from '../NoBorderInput';
 
 const renderNoBorderInputWithProps = (props = {}) => (
@@ -8,66 +13,73 @@ const renderNoBorderInputWithProps = (props = {}) => (
 );
 
 describe('NoBorderInput', () => {
-  const render = createRendererWithDriver(noBorderInputDriverFactory);
-  const createDriver = jsx => render(jsx).driver;
-
-  afterEach(() => {
-    cleanup();
+  describe('[sync]', () => {
+    runTests(createRendererWithDriver(noBorderInputDriverFactory));
   });
 
-  it('should verify component exists', () => {
-    const driver = createDriver(renderNoBorderInputWithProps());
-    expect(driver.exists()).toBe(true);
+  describe('[async]', () => {
+    runTests(createRendererWithUniDriver(noBorderInputUniDriverFactory));
   });
 
-  it('should render label', () => {
-    const someLabel = 'bob';
-    const driver = createDriver(
-      renderNoBorderInputWithProps({ label: someLabel }),
-    );
-    expect(driver.getLabel()).toEqual(someLabel);
-  });
+  function runTests(render) {
+    afterEach(() => {
+      cleanup();
+    });
 
-  it('should not have status', () => {
-    const driver = createDriver(renderNoBorderInputWithProps({}));
-    expect(driver.hasStatus('error')).toBe(false);
-  });
+    it('should verify component exists', async () => {
+      const { driver } = render(renderNoBorderInputWithProps());
+      expect(await driver.exists()).toBe(true);
+    });
 
-  it('should render the status message', () => {
-    const someStatusMessage = 'Error!';
-    const driver = createDriver(
-      renderNoBorderInputWithProps({
-        status: NoBorderInput.StatusError,
-        statusMessage: someStatusMessage,
-      }),
-    );
-    expect(driver.hasStatus('error')).toBe(true);
-    expect(driver.getStatusMessage()).toEqual(someStatusMessage);
-  });
+    it('should render label', async () => {
+      const someLabel = 'bob';
+      const { driver } = render(
+        renderNoBorderInputWithProps({ label: someLabel }),
+      );
+      expect(await driver.getLabel()).toEqual(someLabel);
+    });
 
-  it('should auto focus', () => {
-    const driver = createDriver(
-      renderNoBorderInputWithProps({ autoFocus: true }),
-    );
-    expect(driver.isFocus()).toBe(true);
-  });
+    it('should not have status', async () => {
+      const { driver } = render(renderNoBorderInputWithProps({}));
+      expect(await driver.hasStatus('error')).toBe(false);
+    });
 
-  it('should invoke onFocus', () => {
-    const onFocus = jest.fn();
-    const driver = createDriver(renderNoBorderInputWithProps({ onFocus }));
+    it('should render the status message', async () => {
+      const someStatusMessage = 'Error!';
+      const { driver } = render(
+        renderNoBorderInputWithProps({
+          status: NoBorderInput.StatusError,
+          statusMessage: someStatusMessage,
+        }),
+      );
+      expect(await driver.hasStatus('error')).toBe(true);
+      expect(await driver.getStatusMessage()).toEqual(someStatusMessage);
+    });
 
-    driver.focus();
+    it('should auto focus', async () => {
+      const { driver } = render(
+        renderNoBorderInputWithProps({ autoFocus: true }),
+      );
+      expect(await driver.isFocus()).toBe(true);
+    });
 
-    expect(onFocus).toHaveBeenCalled();
-  });
+    it('should invoke onFocus', async () => {
+      const onFocus = jest.fn();
+      const { driver } = render(renderNoBorderInputWithProps({ onFocus }));
 
-  it('should invoke onBlur', () => {
-    const onBlur = jest.fn();
-    const driver = createDriver(renderNoBorderInputWithProps({ onBlur }));
+      await driver.focus();
 
-    driver.focus();
-    driver.blur();
+      expect(onFocus).toHaveBeenCalled();
+    });
 
-    expect(onBlur).toHaveBeenCalled();
-  });
+    it('should invoke onBlur', async () => {
+      const onBlur = jest.fn();
+      const { driver } = render(renderNoBorderInputWithProps({ onBlur }));
+
+      await driver.focus();
+      await driver.blur();
+
+      expect(onBlur).toHaveBeenCalled();
+    });
+  }
 });
